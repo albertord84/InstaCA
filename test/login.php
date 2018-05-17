3d028b07b94b60c1f3dc2e883a23937915d5bed6
Yordano Pascual <yordanoweb@yahoo.es>
2018-05-17 15:56:21 -0400

    pagina de prueba del logueo mediante guzzle

--- test/login.php
+++ test/login.php
@@ -1,14 +1,35 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 $insta = new \InstaCA\InstaCA;
-
+$username = in_array('username', array_keys($_REQUEST)) ?
+	$_REQUEST['username'] : null;
+$password = in_array('password', array_keys($_REQUEST)) ?
+	$_REQUEST['password'] : null;
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Testing inicio de sesi&oacute;n</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<p><?php echo $insta->userAgent; ?></p>
+	<?php if ($username === null || trim($username) === '') { ?>
+		<div class="login-form">
+			<form method="post">
+				<div class="form-group">
+					<input type="text" class="username" name="username"
+						placeholder="Username" autocomplete="off">
+					<input type="password" class="password" name="password"
+						placeholder="Password" autocomplete="off">
+				</div>
+				<div class="form-footer">
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</div>
+			</form>
+		</div>
+	<?php } ?>
+	<div class="results">
+		<textarea rows="20"><?php if (trim($username) !== '') echo $insta->guzzle(); ?></textarea>
+	</div>
 </body>
 </html>
